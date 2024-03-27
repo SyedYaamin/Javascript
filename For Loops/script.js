c37ee9c99bc5f174de9f69a31f2c35a6e896d4a1
SyedYaamin <156706767+SyedYaamin@users.noreply.github.com>
2024-03-27 10:41:09 +0500

    Add files via upload

---
+++ For Loops/script.js
@@ -0,0 +1,16 @@
+// Question No.1
+// Write a Program to display the message "Hello World" 5 times in your browser using for loops
+
+// for (var i = 1; i <= 5; i++) {
+//     console.log("Hello World", i);
+// }
+// Question No.2
+// Write a program to print numeric counting from 1 to 10
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// Question No. 3
+// Write a program to print multiplication table of any number using for loops. Table number & length should be taken as an input from User.
+
